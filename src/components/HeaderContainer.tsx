803f9a0197531ca5a54e23406c5eb1867b991923
teasun <teasun@wv-works.com>
2022-05-10 04:30:39 +0900

    feat: add blog, education, news Pages

--- src/components/HeaderContainer.tsx
+++ src/components/HeaderContainer.tsx
@@ -2,22 +2,25 @@ import {Button, Image, Menu} from "antd";
 import React from "react";
 import {Header} from "antd/lib/layout/layout";
 import {ArrowRightOutlined} from "@ant-design/icons";
+import Link from "next/link";
 
 const HeaderContainer = ({className} : {className: string}) => {
   return (
       <Header className={className}>
           <Menu mode="horizontal" theme={"dark"}>
               <span style={{float: "left"}}>
-                  <Image src="img/logo.png" preview={false} width={160} style={{margin: 20}}/>
+                  <a href={'/'}>
+                      <Image src="img/logo.png" preview={false} width={160} style={{margin: 20}}/>
+                  </a>
               </span>
               <Menu.Item key="blog" style={{marginLeft: 'auto'}}>
-                  블로그
+                  <Link href={'/blog'}>블로그</Link>
               </Menu.Item>
               <Menu.Item key="news">
-                  뉴스
+                  <Link href={'/news'}>뉴스</Link>
               </Menu.Item>
               <Menu.Item key="education">
-                  교육
+                  <Link href={'/education'}>교육</Link>
               </Menu.Item>
               <Menu.Item key="start">
                   <Button type="primary" shape="round">
---
+++ src/pages/news/index.tsx
@@ -0,0 +1,15 @@
+import Layout from '../../components/layout'
+import React from "react";
+import {Typography} from "antd";
+
+const { Title } = Typography;
+
+export default function News() {
+    return (
+        <Layout>
+            <div className="site-card-border-less-wrapper">
+                <Title style={{color: "white"}}>뉴스 페이지</Title>
+            </div>
+        </Layout>
+    )
+}
